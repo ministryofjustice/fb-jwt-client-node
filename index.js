b1ba611fa2a5b9474ca9062b81c764bc43ae5c49
Alex Robinson <github@alex.fu2k.org>
2019-03-07 21:44:28 +0000

    Change entry point

---
+++ index.js
@@ -0,0 +1,7 @@
+const FBJWTClient = require('lib/fb-jwt-client')
+const FBJWTClientWithSecret = require('lib/fb-jwt-client-with-secret')
+
+module.exports = {
+  FBJWTClient,
+  FBJWTClientWithSecret
+}
